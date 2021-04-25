8d50d6e9ca54d2040a907d5a76ebd69c130aa40e
Shinichi Morimoto <shnmorimoto@gmail.com>
2021-04-25 21:34:36 +0900

    added test operator precedence

--- src/ast.rs
+++ src/ast.rs
@@ -20,33 +20,11 @@ impl fmt::Display for Statement {
             Statement::Let {
                 identifier,
                 expression,
-            } => write!(
-                f,
-                "Let({}, {})",
-                identifier.to_string(),
-                expression.to_string()
-            ),
-            Statement::Return { return_value } => write!(f, "Return({})", return_value.to_string()),
+            } => write!(f, "let {} = {};", identifier, expression),
+            Statement::Return { return_value } => write!(f, "return {};", return_value),
             Statement::Expression { expression } => {
-                write!(f, "Expression({})", expression.to_string())
+                write!(f, "{}", expression)
             }
-        }
-    }
-}
-
-impl Statement {
-    fn to_string(&self) -> String {
-        match self {
-            Statement::Let {
-                identifier,
-                expression,
-            } => format!(
-                "let {} = {};",
-                identifier.to_string(),
-                expression.to_string()
-            ),
-            Statement::Return { return_value } => format!("return {};", return_value),
-            Statement::Expression { expression } => format!("{}", expression.to_string()),
         }
     }
 }
@@ -69,36 +47,17 @@ pub enum Expression {
 impl fmt::Display for Expression {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Expression::Identifier(i) => write!(f, "Identifier({})", i),
-            Expression::IntegerLiteral(i) => write!(f, "IntegralLiteral({})", i),
+            Expression::Identifier(i) => write!(f, "{}", i),
+            Expression::IntegerLiteral(i) => write!(f, "{}", i),
             Expression::PrefixExpression { operator, right } => {
-                write!(f, "PrefixExpression({}, {})", operator, right)
+                write!(f, "({}{})", operator, right)
             }
             Expression::InfixExpression {
                 left,
                 operator,
                 right,
             } => {
-                write!(f, "InfixExpression({}, {}, {})", left, operator, right)
-            }
-        }
-    }
-}
-
-impl Expression {
-    fn to_string(&self) -> String {
-        match self {
-            Expression::Identifier(ident) => format!("{}", &ident.to_string()),
-            Expression::IntegerLiteral(i) => format!("{}", i),
-            Expression::PrefixExpression { operator, right } => {
-                format!("{}{}", operator, right.to_string())
-            }
-            Expression::InfixExpression {
-                left,
-                operator,
-                right,
-            } => {
-                format!("{} {} {}", left.to_string(), operator, right.to_string())
+                write!(f, "({} {} {})", left, operator, right)
             }
         }
     }
@@ -110,10 +69,6 @@ pub struct Ident(pub String);
 impl Ident {
     pub fn new(s: impl Into<String>) -> Self {
         Ident(s.into())
-    }
-
-    pub fn to_string(&self) -> String {
-        self.0.clone()
     }
 }
 
@@ -131,9 +86,15 @@ impl Program {
     pub fn new(statements: Vec<Statement>) -> Self {
         Program { statements }
     }
+}
 
-    pub fn to_string(&self) -> String {
-        format!("{}", self.statements.iter().map(|s| s.to_string()).join(""))
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.statements.iter().map(|s| s.to_string()).join("")
+        )
     }
 }
 
@@ -148,10 +109,10 @@ mod test {
             expression: Expression::Identifier(Ident::new("anotherVar")),
         }]);
         assert_eq!(
-            program.to_string(),
+            format!("{}", program),
             "let myVar = anotherVar;".to_string(),
             "program.to_string() wrong. got = {}",
-            program.to_string()
+            program
         );
     }
 }
